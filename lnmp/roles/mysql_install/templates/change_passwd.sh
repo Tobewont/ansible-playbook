60b3717e50ae2d48e22dd37a3ecb887d52e954e0
Tobewont <42890839+Tobewont@users.noreply.github.com>
2019-04-04 10:38:10 +0800

    Update change_passwd.sh

--- lnmp/roles/mysql_install/templates/change_passwd.sh
+++ lnmp/roles/mysql_install/templates/change_passwd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #该脚本用于更改数据库root密码
 
-passwd=123456789
+passwd={{ MYSQL_PASSWD }}
 n=`grep "/usr/local/mysql/bin" /etc/profile |wc -l`
 
 if [ $n -eq 0 ]
@@ -12,8 +12,8 @@ else
         source /etc/profile
 fi
 
-/usr/local/mysql/bin/mysql -uroot -D mysql -e "UPDATE user SET authentication_string=PASSWORD("$passwd") WHERE user='root';"
+{{ BASE_DIR }}/bin/mysql -uroot -D mysql -e "UPDATE user SET authentication_string=PASSWORD("$passwd") WHERE user='root';"
 
-/usr/local/mysql/bin/mysql -uroot -e "FLUSH PRIVILEGES;"
+{{ BASE_DIR }}/bin/mysql -uroot -e "FLUSH PRIVILEGES;"
 
-/usr/local/mysql/bin/mysql -uroot -p$passwd -e "grant all privileges on *.* to root@'%'  identified by '$passwd';"
+{{ BASE_DIR }}/bin/mysql -uroot -p$passwd -e "grant all privileges on *.* to root@'%'  identified by '$passwd';"
